d209baca043401881c285f4aaa2758610beea87c
Jesse Tuglu <jktuglu@gmail.com>
2021-05-02 22:02:18 -0400

    add paper

--- redblack/src/OrderedMap.java
+++
@@ -1,7 +0,0 @@
-public interface OrderedMap<Key extends Comparable<Key>, Value {
-  boolean isEmpty();
-  int size();
-  OrderedMap<Key, Value> put(Key key, Value value);
-  Value get(Key key);
-  String toString();
-}
