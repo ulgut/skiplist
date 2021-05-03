58d2a04720c3ea2083be2cbb5877169068b525ad
Cole <cadumas01@gmail.com>
2021-05-02 22:10:06 -0400

    Working version of insert

--- skiplist/src/Node.java
+++ skiplist/src/Node.java
@@ -2,71 +2,103 @@ import java.util.ArrayList;
 
 public class Node<Key extends Comparable<Key>, Value> {
 
-	public ArrayList<Node<Key, Value>> prevs; //make private with accessor methods
-	public ArrayList<Node<Key, Value>> nexts; //make private with accessor methods
+  public ArrayList<Node<Key, Value>> prevs; //make private with accessor methods
+  public ArrayList<Node<Key, Value>> nexts; //make private with accessor methods
 
-	private Key key;
-	private Value value;
+  private Key key;
+  private Value value;
 
-
-	public Node(ArrayList<Node<Key, Value>> prevs, ArrayList<Node<Key, Value>> nexts, Key key,
-				Value val) {
-		this.prevs = prevs;
-		this.nexts = nexts;
-		this.key = key;
-		this.value = val;
-	}
-
-	public Node(Key key, Value val) {
-		this(null, null, key, val);
-	}
-
-	public Node() {
-		this(null, null, null, null);
-	}
-
-	public boolean isLess(Node<Key, Value> other) {
-		return this.key.compareTo(other.key) < 0;
-	}
-
-	public int height() {
-		return nexts.size();
-	}
+  enum Type {
+    root,
+    node,
+    cap
+  }
 
 
-	//rewrite with recursion
-	@Override
-	public String toString() {
+  private Type type;
 
-		String str = "";
 
-		for (int i = prevs.size() - 1; i >= 0; i--) {
-			Node<Key, Value> currentNode = this;
+  public Node(ArrayList<Node<Key, Value>> prevs, ArrayList<Node<Key, Value>> nexts, Key key,
+              Value val, Type t) {
+    this.prevs = prevs;
+    this.nexts = nexts;
+    this.key = key;
+    this.value = val;
+    type = t;
+  }
 
-			while (currentNode.nexts.get(i) != null) {
-				str += "{" + currentNode.key + ", " + currentNode.value + "} --> ";
-				currentNode = currentNode.nexts.get(i);
-			}
-			str += "\n";
-		}
+  //initializes a Node with empty prevs and nexts
+  public Node(Key key, Value val, Type t) {
+    this(new ArrayList<Node<Key, Value>>(), new ArrayList<Node<Key, Value>>(), key, val, t);
+  }
 
-		return str;
-	}
+  public Node(Key key, Value val) {
+    this(new ArrayList<Node<Key, Value>>(), new ArrayList<Node<Key, Value>>(), key, val, Type.node);
+  }
 
-	public Key getKey() {
-		return key;
-	}
+  //initializes a Node with empty prevs and nexts, and with key and val as null
 
-	public void setKey(Key key) {
-		this.key = key;
-	}
 
-	public Value getValue() {
-		return value;
-	}
+  public Node(Type t) {
+    this(null, null, t);
+  }
 
-	public void setValue(Value value) {
-		this.value = value;
-	}
+  public Node() {
+    this(Type.node);
+  }
 
+  public boolean isLess(Node<Key, Value> other) {
+    if (this.type == Type.root) {
+      return true;
+    } else if (this.type == Type.cap) {
+      return false;
+    } else {
+      return this.key.compareTo(other.key) < 0;
+    }
+  }
+
+  public int height() {
+    return nexts.size();
+  }
+
+
+  //rewrite with recursion
+  @Override
+  public String toString() {
+
+    String str = "";
+
+    for (int i = nexts.size() - 1; i >= 0; i--) {
+      Node<Key, Value> currentNode = this;
+
+      while (currentNode.type != Type.cap) {
+        str += "{" + currentNode.key + ", " + currentNode.value + "} --> ";
+        currentNode = currentNode.nexts.get(i);
+      }
+      str += "\n";
+    }
+
+    return str;
+  }
+
+
+  public Key getKey() {
+    return key;
+  }
+
+  public void setKey(Key key) {
+    this.key = key;
+  }
+
+  public Value getValue() {
+    return value;
+  }
+
+  public void setValue(Value value) {
+    this.value = value;
+  }
+
+  public Type getType() {
+    return type;
+  }
 }
--- skiplist/src/SkipListC.java
+++ skiplist/src/SkipListC.java
@@ -1,116 +1,146 @@
 public class SkipListC<Key extends Comparable<Key>, Value> implements SkipList<Key, Value> {
 
-	private final double P = 0.5; // 50% chance we increase level.
-	private Node<Key, Value> root; // starting level 1 at smallest key.
-	private int height; // root height
-	private int n;
+  private final double P = 0.5; // 50% chance we increase level.
+  private Node<Key, Value> root; // starting level 1 at smallest key. Acts as -inf
+  private Node<Key, Value> cap; // mirror of root that acts as +inf.
+  private int height; // root height
+  private int n;
 
-	public SkipListC() { // initialize from a single key,value pair
-		this.root = new Node<Key, Value>();
-		this.height = 1;
-		this.n = 0;
-	}
-	//test comment
+  public SkipListC() { // initialize from a single key,value pair
+    this.root = new Node<Key, Value>(Node.Type.root);
+    this.cap = new Node<Key, Value>(Node.Type.cap);
+    this.height = 1;
+    this.n = 0;
+  }
 
-	//    public SkipListC(Key[] keys, Value[] vals) { // initialize from a list of keys and values
-	//        if (keys.length == 0 || keys.length != vals.length) {
-	//            throw new InvalidParameterException("Cannot form a Skip List from these lists.");
-	//        }
-	//
-	//        keys =
-	//        for (int i = 0; i < keys.length; i++) {
-	//            if (i == 0) {
-	//                ///
-	//            }
-	//        }
-	//    }
+  public SkipListC(Key[] keys, Value[] vals) { // initialize from a list of keys and values
+    this();
+    this.insert(keys, vals);
+  }
 
-	public Value get(Key key) {
-		return null;
-	}
+  public Value get(Key key) {
+    return null;
+  }
 
-	public void delete(Key key) {
-		return;
-	}
+  public void delete(Key key) {
+    return;
+  }
 
 
-	public void insert(Key key, Value val) {
+  public void insert(Key key, Value val) {
 
-		int levels = randLevel();
-		System.out.println("Levels: " + levels);
+    int levels = levels();
+    System.out.println("Levels: " + levels);
 
-		//Adjusts height of root / terminal . Make its own method
-		while (levels > height) {
-			root.nexts.add(new Node<>());
-		}
+    //Adjusts height of root / terminal . Make its own method . Same for cap
+    while (levels > root.height()) {
+      root.nexts.add(new Node<>());
+      cap.prevs.add(new Node<>());
 
-		//creating new node
-		Node<Key, Value> newNode = new Node<>(key, val);
+      //linking the root and cap
+      root.nexts.set(root.nexts.size() - 1, cap);
+      cap.prevs.set(root.nexts.size() - 1, root);
 
-		//building new node to proper height
-		for (int i = 0; i < levels; i++) {
-			System.out.println("Built new node at level: " + i);
+    }
 
-			newNode.nexts.add(new Node<Key, Value>());
-			newNode.prevs.add(new Node<Key, Value>());
-		}
+    //creating new node
+    Node<Key, Value> newNode = new Node<Key, Value>(key, val);
 
-		int i = levels;
+    //building new node to proper height
+    for (int i = 0; i < levels; i++) {
+      System.out.println("Built new node at level: " + i);
 
-		//builds of a vertical stack of nodes from top to bottom and links them forward and back progressivley
-		while (i > 0) {
+      newNode.nexts.add(new Node<Key, Value>());
+      System.out.println("newNode.nexts.size is " + newNode.nexts.size());
+      newNode.prevs.add(new Node<Key, Value>());
+    }
 
-			Node<Key, Value> backNode = root;
+    int i = levels - 1;
 
-			//finds previous node at each level to link up to
-			while (backNode.nexts.get(i) != null && backNode.nexts.get(i).isLess(newNode)) {
-				backNode = backNode.nexts.get(i);
-			}
+    //builds of a vertical stack of nodes from top to bottom and links them forward and back progressivley
+    while (i >= 0) {
+      Node<Key, Value> backNode = root;
 
-			Node<Key, Value> frontNode = backNode.nexts.get(i);
+      while (i < backNode.height() && backNode.nexts.get(i) != cap && backNode.nexts.get(i).isLess(newNode)) {
+        backNode = backNode.nexts.get(i);
+      }
 
-			//linking backNode to newNode
-			newNode.prevs.set(i, backNode);
-			backNode.nexts.set(i, newNode);
+      //Setting the front node
+      Node<Key, Value> frontNode = cap;
+      if (backNode.nexts.get(i) != null)
+        frontNode = backNode.nexts.get(i);
 
-			//linking frontNode to newNode
-			frontNode.prevs.set(i, newNode);
-			newNode.nexts.set(i, frontNode);
+      //linking backNode to newNode
+      newNode.prevs.set(i, backNode);
+      backNode.nexts.set(i, newNode);
 
-			i--;
-		}
-		n++;
-	}
+      //linking frontNode to newNode
+      frontNode.prevs.set(i, newNode);
+      newNode.nexts.set(i, frontNode);
 
+      i--;
+    }
+    n++;
+  }
 
-	private int randLevel() {
-		System.out.println("rand level called");
-		if (Math.random() < P)
-			return 1 + randLevel();
-		else
-			return 1; //make 1?
-	}
+  public void insert(Key[] keys, Value[] vals) {
+    if (keys.length != vals.length)
+      throw new IndexOutOfBoundsException("Invalid Input! keys and vals arrays must have same number of elements");
+    else
+      for (int i = 0; i < keys.length; i++) {
+        insert(keys[i], vals[i]);
+      }
 
+  }
 
-	public boolean isEmpty() {
-		return false;
-	}
-
-	public int size() {
-		return n;
-	}
-
-	@Override
-	public String toString() {
-		return root.toString();
-	}
-
-	public static void main(String[] args) {
-		SkipListC<Integer, String> sl = new SkipListC<>();
+  //generates a number of levels for a node to have
+  private int levels() {
+    if (Math.random() < P)
+      return 1 + levels();
+    else
+      return 1; // Min number is 1
+  }
 
 
-		sl.insert(5, "Hello");
-		System.out.println(sl.toString());
+  public boolean isEmpty() {
+    return false;
+  }
 
-	}
+  public int size() {
+    return n;
+  }
+
+  @Override
+  public String toString() {
+    return root.toString();
+  }
+
+  public static void main(String[] args) {
+    SkipListC<Integer, String> sl = new SkipListC<>();
+
+    sl.insert(6, "Testing");
+    sl.insert(5, "Hello");
+    sl.insert(1, "Before Hello");
+    sl.insert(-10000, "wicked early");
+    sl.insert(5, "Second 5");
+    sl.insert(1000, "Wicked big");
+    sl.insert(500, "pretty big");
+    sl.insert(30, "Medium");
+    System.out.println(sl);
+
+    SkipListC<Integer, String> sl2 = new SkipListC<>();
+
+    int n = 10;
+    Integer[] keys = new Integer[n];
+    String[] vals = new String[n];
+
+    for (int i = 0; i < n; i++) {
+      keys[i] = (int) (1000 * Math.random());
+      vals[i] = "This is val #" + i;
+    }
+
+    System.out.println("\n\n Printing the SkipList:");
+    sl2.insert(keys, vals);
+    System.out.println(sl2);
+  }
 }
