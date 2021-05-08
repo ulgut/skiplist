8b1ae6cc6e7830ae46fb9833e248d4a37542b44e
Cole <cadumas01@gmail.com>
2021-05-08 15:03:39 -0400

    Working on preventing duplicate inserts

--- skiplist/src/SeqNode.java
+++ skiplist/src/SeqNode.java
@@ -2,103 +2,106 @@ import java.util.ArrayList;
 
 public class SeqNode<Key extends Comparable<Key>, Value> {
 
-	public ArrayList<SeqNode<Key, Value>> prevs; //make private with accessor methods
-	public ArrayList<SeqNode<Key, Value>> nexts; //make private with accessor methods
+  public ArrayList<SeqNode<Key, Value>> prevs; //make private with accessor methods
+  public ArrayList<SeqNode<Key, Value>> nexts; //make private with accessor methods
 
-	private Key key;
-	private Value value;
+  private Key key;
+  private Value value;
 
-	enum Type {
-		root,
-		node,
-		cap
-	}
-
-
-	private Type type;
+  enum Type {
+    root,
+    node,
+    cap
+  }
 
 
-	public SeqNode(ArrayList<SeqNode<Key, Value>> prevs, ArrayList<SeqNode<Key, Value>> nexts, Key key,
-				   Value val, Type type) {
-		this.prevs = prevs;
-		this.nexts = nexts;
-		this.key = key;
-		this.value = val;
-		this.type = type;
-	}
+  private Type type;
 
-	//initializes a Node with empty prevs and nexts
-	public SeqNode(Key key, Value val, Type t) {
-		this(new ArrayList<SeqNode<Key, Value>>(), new ArrayList<SeqNode<Key, Value>>(), key, val, t);
-	}
 
-	public SeqNode(Key key, Value val) {
-		this(new ArrayList<SeqNode<Key, Value>>(), new ArrayList<SeqNode<Key, Value>>(), key, val, Type.node);
-	}
+  public SeqNode(ArrayList<SeqNode<Key, Value>> prevs, ArrayList<SeqNode<Key, Value>> nexts, Key key,
+                 Value val, Type type) {
+    this.prevs = prevs;
+    this.nexts = nexts;
+    this.key = key;
+    this.value = val;
+    this.type = type;
+  }
 
-	public SeqNode(Type t) {
-		this(null, null, t);
-	}
+  //initializes a Node with empty prevs and nexts
+  public SeqNode(Key key, Value val, Type t) {
+    this(new ArrayList<SeqNode<Key, Value>>(), new ArrayList<SeqNode<Key, Value>>(), key, val, t);
+  }
 
-	public SeqNode() {
-		this(Type.node);
-	}
+  public SeqNode(Key key, Value val) {
+    this(new ArrayList<SeqNode<Key, Value>>(), new ArrayList<SeqNode<Key, Value>>(), key, val, Type.node);
+  }
 
-	public boolean isLess(SeqNode<Key, Value> other) {
-		if (this.type == Type.root)
-			return true;
-		else if (this.type == Type.cap)
-			return false;
-		else
-			return this.key.compareTo(other.key) < 0;
-	}
+  public SeqNode(Type t) {
+    this(null, null, t);
+  }
 
-	public boolean isLess(Key otherKey) {
-		if (this.type == Type.root)
-			return true;
-		else if (this.type == Type.cap)
-			return false;
-		else
-			return this.key.compareTo(otherKey) < 0;
-	}
+  public SeqNode() {
+    this(Type.node);
+  }
 
-	public boolean equals(Key otherKey) {
-		if (this.key == null)
-			return false;
-		else {
-			return this.key.compareTo(otherKey) == 0;
-		}
-	}
+  public boolean isLess(SeqNode<Key, Value> other) {
+    if (this.type == Type.root)
+      return true;
+    else if (this.type == Type.cap)
+      return false;
+    else
+      return this.key.compareTo(other.key) < 0;
+  }
 
-	public int height() {
-		return nexts.size();
-	}
+  public boolean isLess(Key otherKey) {
+    if (this.type == Type.root)
+      return true;
+    else if (this.type == Type.cap)
+      return false;
+    else
+      return this.key.compareTo(otherKey) < 0;
+  }
 
-	//rewrite with recursion
-	@Override
-	public String toString() {
+  public boolean equals(Key otherKey) {
+    if (this.key == null)
+      return false;
+    else {
+      return this.key.compareTo(otherKey) == 0;
+    }
+  }
 
-		String str = "";
+  public int height() {
+    return nexts.size();
+  }
 
-		for (int i = nexts.size() - 1; i >= 0; i--) {
-			SeqNode<Key, Value> currentNode = this;
+  @Override
+  public String toString() {
 
-			while (currentNode.type != Type.cap) {
-				str += "{" + currentNode.key + ", " + currentNode.value + "} --> ";
-				currentNode = currentNode.nexts.get(i);
-			}
-			str += "\n";
-		}
+    String str = "";
 
-		return str;
-	}
+    for (int i = nexts.size() - 1; i >= 0; i--) {
+      SeqNode<Key, Value> currentNode = this;
 
-	public Value getValue() {
-		return value;
-	}
+      while (currentNode.type != Type.cap) {
+        str += "{" + currentNode.key + ", " + currentNode.value + "} --> ";
+        currentNode = currentNode.nexts.get(i);
+      }
+      str += "\n";
+    }
 
-	public Type getType() {
-		return type;
-	}
+    return str;
+  }
+
+  public Key getKey() {
+    return key;
+  }
+
+  public Value getValue() {
+    return value;
+  }
+
+  public Type getType() {
+    return type;
+  }
 
 }
--- skiplist/src/Tests.java
+++ skiplist/src/Tests.java
@@ -1,118 +1,156 @@
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 
 public class Tests {
 
-	private static class Log {
-		public void print(String test, long time, String units) {
-			System.out.println(String.format("%s: %d%s", test, time, units));// millis
-		}
-	}
+  private static class Log {
+    public void print(String test, long time, String units) {
+      System.out.println(String.format("%s: %d%s", test, time, units));// millis
+    }
+  }
 
-	private static class Record {
-		HashMap<String, Long> hm;
+  private static class Record {
+    HashMap<String, Long> hm;
 
-		public Record() {
-			this.hm = new HashMap<String, Long>();
-		}
+    public Record() {
+      this.hm = new HashMap<String, Long>();
+    }
 
-		public int size() {
-			return hm.size();
-		}
+    public int size() {
+      return hm.size();
+    }
 
-		public long avg() {
-			long sum = hm.values().stream().mapToLong(val -> val).sum();
-			return sum / this.size();
-		}
+    public long avg() {
+      long sum = hm.values().stream().mapToLong(val -> val).sum();
+      return sum / this.size();
+    }
 
-		public void add(String test, long result) {
-			hm.put(test, result);
-		}
-	}
+    public void add(String test, long result) {
+      hm.put(test, result);
+    }
+  }
 
-	public static void test(int n) {
-		System.out.println("\n%%%%%%%%%%%%Test with " + n + " elements%%%%%%%%%%%%");
-		Log log = new Log();
-		Integer[] keys = new Integer[n];
-		String[] vals = new String[n];
+  public static void test(int n) {
+    System.out.println("\n%%%%%%%%%%%%Test with " + n + " elements%%%%%%%%%%%%");
+    Log log = new Log();
+    Integer[] keys = new Integer[n];
+    String[] vals = new String[n];
 
-		for (int i = 0; i < n; i++) {
-			int key = (int) (1000000 * Math.random());
-			keys[i] = key;
-			vals[i] = "TEST#" + key;
-		}
-		long startTime;
-		long endTime;
-
-
-		// Initialization
-		startTime = System.nanoTime();
-		SkipListSeqC<Integer, String> seqSl = new SkipListSeqC<>(keys, vals);
-		endTime = System.nanoTime();
-		log.print("Sequential Initialization", (endTime - startTime), "ns");
-
-		startTime = System.nanoTime();
-		SkipListLinkedC<Integer, String> linkedSl = new SkipListLinkedC<>(keys, vals);
-		endTime = System.nanoTime();
-		log.print("Linked Initialization", (endTime - startTime), "ns");
-		// Initialization End
-
-		// Get
-		Record seqGetRecord = new Record();
-		for (int i = 0; i < keys.length; i++) {
-			startTime = System.nanoTime();
-			String val = seqSl.get(keys[i]);
-			endTime = System.nanoTime();
-			seqGetRecord.add("Get Test:" + i, endTime - startTime);
-			assert (val.equals(vals[i]));
-
-		}
-		log.print("Sequential GET", seqGetRecord.avg(), "ns");
+    for (int i = 0; i < n; i++) {
+      int key = (int) (1000000 * Math.random());
+      keys[i] = key;
+      vals[i] = "TEST#" + key;
+    }
+    long startTime;
+    long endTime;
 
 
-		Record linkedGetRecord = new Record();
-		for (int i = 0; i < keys.length; i++) {
-			startTime = System.nanoTime();
-			String val = linkedSl.get(keys[i]);
-			endTime = System.nanoTime();
-			linkedGetRecord.add("Get Test:" + i, endTime - startTime);
-			assert (val.equals(vals[i]));
+    // Initialization
+    startTime = System.nanoTime();
+    SkipListSeqC<Integer, String> seqSl = new SkipListSeqC<>(keys, vals);
+    endTime = System.nanoTime();
+    log.print("Sequential Initialization", (endTime - startTime), "ns");
 
-		}
-		log.print("Linked GET", linkedGetRecord.avg(), "ns");
-		// Get End
+    startTime = System.nanoTime();
+    SkipListLinkedC<Integer, String> linkedSl = new SkipListLinkedC<>(keys, vals);
+    endTime = System.nanoTime();
+    log.print("Linked Initialization", (endTime - startTime), "ns");
+    // Initialization End
 
-		//Delete
-		Record seqDeleteRecord = new Record();
-		for (int i = 0; i < keys.length; i++) {
-			startTime = System.nanoTime();
-			seqSl.delete(keys[i]);
-			endTime = System.nanoTime();
-			seqDeleteRecord.add("Get Test:" + i, endTime - startTime);
+    // Get
+    Record seqGetRecord = new Record();
+    for (int i = 0; i < seqSl.size(); i++) {
+      startTime = System.nanoTime();
+      String val = seqSl.get(keys[i]);
+      endTime = System.nanoTime();
+      seqGetRecord.add("Get Test:" + i, endTime - startTime);
+      assert (val.equals(vals[i]));
 
-		}
-		log.print("Sequential DELETE", seqDeleteRecord.avg(), "ns");
-
-
-		Record linkedDeleteRecord = new Record();
-		for (int i = 0; i < keys.length; i++) {
-			startTime = System.nanoTime();
-			linkedSl.delete(keys[i]);
-			endTime = System.nanoTime();
-			linkedDeleteRecord.add("Get Test:" + i, endTime - startTime);
-
-		}
-		log.print("Linked DELETE", linkedDeleteRecord.avg(), "ns");
-
-		assert (seqSl.size() == linkedSl.size() && linkedSl.size() == 0);
-		//Delete End
-
-	}
+    }
+    log.print("Sequential GET", seqGetRecord.avg(), "ns");
 
 
-	public static void main(String[] args) {
-		test(10); //10 elements
-		test(100); //100 elements
-		test(1000); //100 elements
-		test(10000); //10k elements
-	}
+    Record linkedGetRecord = new Record();
+    for (int i = 0; i < linkedSl.size(); i++) {
+      startTime = System.nanoTime();
+      String val = linkedSl.get(keys[i]);
+      endTime = System.nanoTime();
+      linkedGetRecord.add("Get Test:" + i, endTime - startTime);
+      assert (val.equals(vals[i]));
+
+    }
+    log.print("Linked GET", linkedGetRecord.avg(), "ns");
+    // Get End
+
+    //Delete
+    Record seqDeleteRecord = new Record();
+    for (int i = 0; i < seqSl.size(); i++) {
+      startTime = System.nanoTime();
+      try {
+        seqSl.delete(keys[i]);
+      } catch (NoSuchElementException e) {
+      }
+      endTime = System.nanoTime();
+      seqDeleteRecord.add("Get Test:" + i, endTime - startTime);
+    }
+    log.print("Sequential DELETE", seqDeleteRecord.avg(), "ns");
+
+
+    Record linkedDeleteRecord = new Record();
+    for (int i = 0; i < seqSl.size(); i++) {
+      startTime = System.nanoTime();
+      try {
+        seqSl.delete(keys[i]);
+      } catch (NoSuchElementException e) {
+      }
+      endTime = System.nanoTime();
+      linkedDeleteRecord.add("Get Test:" + i, endTime - startTime);
+
+    }
+    log.print("Linked DELETE", linkedDeleteRecord.avg(), "ns");
+
+    assert (seqSl.size() == linkedSl.size() && linkedSl.size() == 0);
+    //Delete End
+
+  }
+
+
+  public static void main(String[] args) {
+
+    SkipListSeqC<Integer, String> sl = new SkipListSeqC<>();
+
+    int n = 100;
+    Integer[] keys = new Integer[n];
+    String[] vals = new String[n];
+
+    for (int i = 0; i < n; i++) {
+      keys[i] = (int) (1000 * Math.random());
+      vals[i] = "This is val #" + i;
+    }
+
+    sl.insert(keys, vals);
+    sl.insert(4000, "My Key");
+    System.out.println(sl);
+    System.out.println("sl size: " + sl.size());
+    System.out.println("Testing out get(700): " + sl.get(4000));
+    System.out.printf("There were %d comparisons in the search", sl.getComparisons());
+    sl.resetComparisons();
+
+    System.out.println("\n\n---- About to Delete Key = 400 ---\n");
+
+    System.out.println("sl size before deletion: " + sl.size());
+    sl.delete(4000);
+    System.out.println("sl size after deletion: " + sl.size());
+    try {
+      sl.get(4000);
+    } catch (NoSuchElementException e) {
+      System.out.println("The node with key = 4000 was successfully deleted");
+    }
+
+
+    test(10); //10 elements
+    test(100); //100 elements
+    test(1000); //1000 elements
+    test(10000); //10k elements
+  }
 }
